2244f80a9cb1ae944a6b0387aafae0f10cd31002
Quaquoum <55735086+Quaquoum@users.noreply.github.com>
2021-01-22 12:56:49 +0100

    Ennemy follow

--- PunkCybernetique2076/Assets/Scripts/Characters/Ennemy.cs
+++ PunkCybernetique2076/Assets/Scripts/Characters/Ennemy.cs
@@ -7,20 +7,52 @@ using UnityEngine.AI;
 public class Ennemy : LivingEntities
 {
     [SerializeField] private Player player;
-    [SerializeField] private Transform characterTransform;
+
+
+    public float lookRadius = 30f;
+
+    Transform target;
+    NavMeshAgent agent;
 
     private void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();
         CallAwake();
     }
 
     private void Start()
     {
+        target = player.transform;
+        
     }
 
-    void Update()
+    private void Update()
     {
+        float distance = Vector3.Distance(target.position, transform.position);
 
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                //attack
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 
     private void OnCollisionEnter(Collision collision)
