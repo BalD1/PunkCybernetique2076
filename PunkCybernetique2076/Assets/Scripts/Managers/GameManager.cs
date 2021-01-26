25264ea049e27c8a4aa0930f51be9c4f7265d7df
Florian <florianhusson@hotmail.fr>
2021-01-26 17:20:38 +0100

    Revert "test"
    This reverts commit 409082b71b09c2c76498dbacc931755dbbcd70b3.

--- PunkCybernetique2076/Assets/Scripts/Managers/GameManager.cs
+++ PunkCybernetique2076/Assets/Scripts/Managers/GameManager.cs
@@ -130,7 +130,6 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator LoadAsyncOperation()
     {
-        //test
         yield return null;
         AsyncOperation gameLoad = SceneManager.LoadSceneAsync("MainScene");
         gameLoad.allowSceneActivation = false;
